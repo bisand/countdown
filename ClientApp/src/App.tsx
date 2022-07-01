5c7a07ea61fd0229a34285c348fd54b586360a1a
André Biseth <andre.biseth@onyxcentersource.com>
2022-07-01 09:46:17 +0200

    Testing local debug

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in 123
         </a>
       </header>
     </div>
